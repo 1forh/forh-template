e976d4a224ed892a40401c53866fb841cc8cb78e
1forh <1forhlol@gmail.com>
2016-01-04 10:25:29 -0500

    Remove whitespace from sitemap task

--- gulpfile.js/tasks/sitemap.js
+++ gulpfile.js/tasks/sitemap.js
@@ -4,10 +4,10 @@ var gulp = require('gulp');
 var sitemap = require('gulp-sitemap');
  
 gulp.task('sitemap', function () {
-    gulp.src([config.paths.build + '*.html', '!' + config.paths.build + 'google*.html'])
-        .pipe(sitemap({
-            siteUrl: config.sitemap.siteUrl,
-            changefreq: config.sitemap.changeFreq
-        }))
-        .pipe(gulp.dest(config.paths.dist));
+  gulp.src([config.paths.build + '*.html', '!' + config.paths.build + 'google*.html'])
+    .pipe(sitemap({
+        siteUrl: config.sitemap.siteUrl,
+        changefreq: config.sitemap.changeFreq
+    }))
+    .pipe(gulp.dest(config.paths.dist));
 });
