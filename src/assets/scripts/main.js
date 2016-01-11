73e388f8c864639770cc43d16e185ebf9c2661ba
Zachary Minner <1forhlol@gmail.com>
2016-01-11 15:45:09 -0500

    Design new home page

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -4,4 +4,35 @@ $(document).foundation();
 
 (function($){
 
+  // Adds padding-top to #hero based on the height of #site-header
+  var heroMargin = function(){
+    var hero = $('#hero');
+    var header = $('#site-header');
+    var height = header.height();
+
+    hero.css('padding-top', height + 50);
+
+  }();
+
+  // Adds margin-top to #first-section based on the height of #hero
+  var firstSectionMargin = function(){
+    var sec = $('#first-section');
+    var hero = $('#hero');
+    var height = hero.height();
+
+    sec.css('margin-top', height + 100);
+  }();
+
+  // Stop scroll on mouse over map
+  $('#map_canvas').addClass('scrolloff'); 
+  $('#map').on('click', function () {
+      $('#map_canvas').removeClass('scrolloff'); 
+  });
+
+  $("#map_canvas").mouseleave(function () {
+      $('#map_canvas').addClass('scrolloff');
+  });
+
+
+
 })(jQuery);
