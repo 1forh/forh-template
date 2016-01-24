f0d7c4eee58a658ce796714544efd080477c7a59
Zachary Minner <1forhlol@gmail.com>
2016-01-24 18:18:35 -0500

    Update gulpfile/index

--- gulpfile.js/index.js
+++ gulpfile.js/index.js
@@ -2,10 +2,7 @@
 
 // This configuration follows the modular design of the `gulp-starter` project by Dan Tello: https://github.com/greypants/gulp-starter
 // Explore `tasks` for more...
-
 var requireDir = require('require-dir');
 
-requireDir('./tasks/dev');
-requireDir('./tasks/prod');
-requireDir('./tasks/deploy');
-requireDir('./tasks');
+// {recurse: true} requires all sub directories as well
+requireDir('./tasks', {recurse: true});
