551cdf7e8f74ffdd7a5b2d3049edcf9190cc4a39
1forh <1forhlol@gmail.com>
2016-02-23 17:39:22 -0500

    Removed slick slider

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -3,16 +3,6 @@
 // Call Foundation
 // $(document).foundation();
 
-// Call Slick -- http://kenwheeler.github.io/slick/
-// $(document).ready(function(){
-//   $('.forh-slider').slick({
-//     dots: true,
-//     infinite: true,
-//     autoplay: true,
-//     autoplaySpeed: 8000
-//   });
-// });
-
 // Add custom scripts here
 (function($){
 
