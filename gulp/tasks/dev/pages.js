f07c3bd2ea560171f4fcb4ea6781ab988cbcf72e
1forh <1forhlol@gmail.com>
2016-03-09 08:46:27 -0500

    Add php file extension to pages task

--- gulp/tasks/dev/pages.js
+++ gulp/tasks/dev/pages.js
@@ -6,7 +6,7 @@ module.exports = function(gulp, config) {
 
   // Use panini to copy page templates into finished HTML
   gulp.task('pages', function() {
-    gulp.src(config.paths.src + 'pages/**/*.{html,hbs,handlebars}')
+    gulp.src(config.paths.src + 'pages/**/*.{html,hbs,handlebars,php}')
       .pipe(panini ({
           root: config.paths.src + 'pages/',
           layouts: config.paths.src + 'layouts/',
