51013f0540c0ee1921b9b3b5dcc499ef38c88cc0
1forh <1forhlol@gmail.com>
2016-03-23 08:10:41 -0400

    Removed babel from tasks and package.json, the modules were removed from NPM

--- gulp/tasks/prod/production.js
+++ gulp/tasks/prod/production.js
@@ -4,7 +4,6 @@ var sequence = require('run-sequence');
 var browser = require( 'browser-sync' );
 var cssnano = require('gulp-cssnano');
 var purify = require('gulp-purifycss');
-var babel = require('gulp-babel');
 var uglify = require('gulp-uglify');
 var imagemin = require('gulp-imagemin');
 
@@ -29,9 +28,6 @@ module.exports = function(gulp, config) {
 
   gulp.task('dist:javascript', ['javascript'], function(){
     return gulp.src(config.paths.build + 'assets/scripts/*.js')
-      .pipe(babel({
-        presets: [ 'es2015' ]
-      }))
       .pipe(uglify())
       .pipe(gulp.dest(config.paths.dist + 'assets/scripts/'));
   });
