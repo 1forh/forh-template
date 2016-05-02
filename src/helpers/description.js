ec6f7e9115fd09cab7bdb38550b52de2298e7b03
1forh <1forhlol@gmail.com>
2016-05-01 23:22:20 -0400

    Fixed comments

--- src/helpers/description.js
+++ src/helpers/description.js
@@ -2,11 +2,11 @@ var format = require('util').format;
 var config = require( "../../gulp/config.js" );
 
 /**
- * Handlebars block helper that displays meta content for current page using data/config.json
+ * Handlebars block helper that displays meta description for current page using data/config.json
  * @param {string} page - Current page
  * @example
- * {{#title 'index'}}{{/title}}
- * @returns The HTML inside the helper, displaying the title tag for each page.
+ * {{#description 'index'}}{{/description}}
+ * @returns The HTML inside the helper, displaying the meta description for each page.
  */
 
 module.exports = function(page) {
--- src/helpers/title.js
+++ src/helpers/title.js
@@ -2,11 +2,11 @@ var format = require('util').format;
 var config = require( "../../gulp/config.js" );
 
 /**
- * Handlebars block helper that displays meta content for current page using data/config.json
+ * Handlebars block helper that displays title tag contents and contents for current page using data/config.json
  * @param {string} page - Current page
  * @example
  * {{#title 'index'}}{{/title}}
- * @returns The HTML inside the helper, displaying the title tag for each page.
+ * @returns The HTML inside the helper, displaying the title tag and contents for each page.
  */
 
 module.exports = function(page) {
