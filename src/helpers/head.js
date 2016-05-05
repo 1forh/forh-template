105c126f152ad999d50a38ce15a5a33e6c732937
1forh <1forhlol@gmail.com>
2016-05-04 23:38:08 -0400

    Updated comments in head.js helper

--- src/helpers/head.js
+++ src/helpers/head.js
@@ -2,23 +2,25 @@ var format = require('util').format;
 var config = require( "../../config.js" ).pages;
 
 /**
- * Handlebars block helper that displays head information for current page using config.js
+ * Handlebars block helper
+ * Displays head information for current page using config.js
+ *
  * @param {string} option - Selects which config option to use from config.js
  * @param {object} page - Handlebars object
  * @example
  * {{#head 'title'}}{{page}}{{/head}}
- * @returns the config option selected for current page
+ * @returns the config option selected for the current page
  */
 module.exports = function(option, page) {
 
-  for( var i = 0; i  < config.length; i++) {
-    for( var key in config[i] ) {
-      if( config[i][key]['name'] == page.fn(this) ) {
+  for(var i = 0; i  < config.length; i++) {
+    for(var key in config[i]) {
+      if(config[i][key]['name'] == page.fn(this)) {
         option = config[i][key][option];
       }
     }
   }
 
-  return format( '%s', option );
+  return format('%s', option);
 
 }
